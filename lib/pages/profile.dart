929817dd32a667609590b777a077ebff72569f78
rifkytecoder <rifkytecoder@gmail.com>
2022-11-05 15:44:17 +0800

    create extract widget circle StoryItem and Row using SingleChildScrollView axis horizontal

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -126,6 +126,87 @@ class ProfilePage extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(
+            height: 8,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            // todo Circle Story
+            // agar bisa scroll ke kanan/horizontal
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: const [
+                  // todo StoryItem extract widget
+                  StoryItem("Story 1"),
+                  StoryItem("Story 2"),
+                  StoryItem("Story 3"),
+                  StoryItem("Story 4"),
+                  StoryItem("Story 5"),
+                  StoryItem("Story 6"),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// todo StoryItem Extract Widget
+class StoryItem extends StatelessWidget {
+  final String title;
+
+  // todo constructor (name: argument)
+  // const StoryItem({
+  //   Key? key,
+  //   required this.title
+  // }) : super(key: key);
+  const StoryItem(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8),
+      child: Column(
+        children: [
+          // todo Circle stack container story
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(80 / 2),
+                ),
+              ),
+              Container(
+                width: 77,
+                height: 77,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(77 / 2),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 5,
+                  ),
+                  // photo NetworkImage
+                  image: const DecorationImage(
+                    image: NetworkImage("https://picsum.photos/200"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          // const Text("Story 1"),
+          Text(title),
         ],
       ),
     );
